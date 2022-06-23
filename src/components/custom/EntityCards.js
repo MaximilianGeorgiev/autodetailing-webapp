00491d70a4a77c3e6d8b28ad56840091026b4b2d
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2022-06-23 13:27:16 +0300

    fix: Rework entity cards

--- src/components/custom/EntityCards.js
+++ src/components/custom/EntityCards.js
@@ -9,6 +9,7 @@ import { useState, useEffect } from 'react';
 
 import { getAllServices, getServicePicturePaths } from "../../api/service";
 import { getAllProducts, getProductPicturePaths } from "../../api/product";
+import { useNavigate } from 'react-router-dom';
 
 // props:
 // entityType: product/service
@@ -19,23 +20,23 @@ export const EntityCards = (props) => {
   const [entityPictures, setEntityPictures] = useState([]);
   const [picturesLoaded, setPicturesLoaded] = useState(false);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
+    let updatedPaths = [...entityPictures];
+    let entities = [];
+
     if (props.entityType === "service") {
       getAllServices().then((res) => {
         if (res.data.status === "success") {
           const services = res.data.payload.filter((val, index, arr) => index + 3 >= arr.length);
-          setEntities(services);
+          entities = services;
 
 
           for (let i = 0; i < services.length; i++) {
             getServicePicturePaths(services[i].service_id).then((res) => {
               if (res.data?.status === "success") {
-                let updatedPaths = [...entityPictures];
-
                 updatedPaths.push({ id: services[i].service_id, path: res.data.payload[0].picture_path }); // take one picture for thumbnail
-
-                setEntityPictures(updatedPaths);
-                setPicturesLoaded(true);
               }
             });
           };
@@ -47,8 +48,7 @@ export const EntityCards = (props) => {
       getAllProducts().then((res) => {
         if (res.data.status === "success") {
           const products = res.data.payload.filter((val, index, arr) => index + 3 >= arr.length);
-
-          setEntities(products);
+          entities = products;
         }
       });
 
@@ -56,16 +56,17 @@ export const EntityCards = (props) => {
       for (let i = 0; i < entities.length; i++) {
         getProductPicturePaths(entities[i].product_id).then((res) => {
           if (res.data?.status === "success") {
-            let updatedPaths = [...entityPictures];
-
             updatedPaths.push({ id: entities[i].product_id, path: res.data.payload[0].picture_path }); // take one picture for thumbnail
-
-            setEntityPictures(updatedPaths);
-            setPicturesLoaded(true);
           }
         });
       };
     }
+
+    setTimeout(() => {
+      setEntityPictures(updatedPaths);
+      setEntities(entities);
+      setPicturesLoaded(true);
+    }, 1500);
   }, []);
 
   const displayTitle = (id) => {
@@ -80,40 +81,45 @@ export const EntityCards = (props) => {
   const displayPrice = (id) => {
     if (!id) return "";
 
-    if (props.entityType === "service") return entities.filter(e => e.service_id === id)[0].service_price;
-    else if (props.entityType === "product") return entities.filter(e => e.product_id === id)[0].product_price;
+    if (props.entityType === "service") return entities.filter(e => e.service_id === id)[0].service_price + " BGN";
+    else if (props.entityType === "product") return entities.filter(e => e.product_id === id)[0].product_price + " BGN";
 
+    return "";
   };
 
-  return (
-    <ImageList sx={{ width: 999, height: 999 }}>
-      <ImageListItem key="Subheader" cols={2}>
-        <ListSubheader component="div">December</ListSubheader>
-      </ImageListItem>
 
-      {picturesLoaded === true && (entityPictures.map((pic) => {
-        return (<ImageListItem key={pic.path}>
-          <img
-            src={`${pic.path}?w=248&fit=crop&auto=format`}
-            srcSet={`${pic.path}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt=""
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={() => displayTitle(pic.id)}
-            subtitle={() => displayPrice(pic.id)}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${pic.path}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>)
-}))
-      }
+  return (
+    <>
+    {picturesLoaded && <ImageList sx={{ width: 500, height: 450 }}>
+    <ImageListItem key="Subheader" cols={2}>
+      <ListSubheader component="div">December</ListSubheader>
+    </ImageListItem>
+    {entityPictures.map((pic) => (
+      <ImageListItem key={pic.path}>
+        <img
+          src={`${pic.path}?w=248&fit=crop&auto=format`}
+          srcSet={`${pic.path}?w=248&fit=crop&auto=format&dpr=2 2x`}
+          alt={""}
+          loading="lazy"
+        />
+        <ImageListItemBar
+          title={displayTitle(pic.id)}
+          subtitle={displayPrice(pic.id)}
+          actionIcon={
+            <IconButton
+              sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+              aria-label={`info about ${pic.id}`}
+              onClick={() => {navigate(`/${props.entityType}s/show/${pic.id}`)}}
+            >
+              <InfoIcon />
+            </IconButton>
+          }
+        />
+      </ImageListItem>
+    ))}
     </ImageList>
+    }
+    </>
   );
+
 }
--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import { ShowBlog } from "./components/show/ShowBlog";
 import { ProductTable } from "./components/tables/ProductTable";
 import { ServiceTable } from "./components/tables/ServiceTable";
 import { PromotionTable } from "./components/tables/PromotionTable";
+import { EntityCards} from "./components/custom/EntityCards";
 
 import { App } from "./App";
 
@@ -52,6 +53,8 @@ root.render(
           <Route path="/products/show/all" element={<ProductTable />} />
           <Route path="/services/show/all" element={<ServiceTable />} />
           <Route path="/promotions/show/all" element={<PromotionTable />} />
+          <Route path="/test" element={<EntityCards entityType="service" />} />
+          <Route path="/test2" element={<EntityCards entityType="product" />} />
         </Routes>
         <App />
       </BrowserRouter>
